b82ccdb129f2b8eab51917be8d296e05f4290cb6
Dmitry Pavlushin <pavlushindv@gmail.com>
2023-12-24 19:56:47 +0500

    pay_rate correct

--- homework-1/main.py
+++ homework-1/main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     print(item2.calculate_total_price())  # 100000
 
     # устанавливаем новый уровень цен
-    Item.pay_rate = 0.8
+    Item.pay_rate = 0.85
     # применяем скидку
     item1.apply_discount()
 
