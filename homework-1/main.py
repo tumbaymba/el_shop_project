da0edda861356c640f1ff8b4ad229c3a84e4b8cb
Dmitry Pavlushin <pavlushindv@gmail.com>
2023-12-27 16:05:23 +0500

    HW 13.2

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,8 +1,8 @@
 from src.item import Item
 
 if __name__ == '__main__':
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
+    item1 = Item("Смартфон", 10000, 100)
+    item2 = Item("Ноутбук", 20000, 50)
 
     print(item1.calculate_total_price())  # 200000
     print(item2.calculate_total_price())  # 100000
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,10 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
 from src.item import Item
 
 item1 = Item("Смартфон", 10000, 100)
 item2 = Item("Ноутбук", 20000, 50)
 item1.pay_rate = 0.85
 item2.pay_rate = 0.85
+
+@pytest.fixture
+def gadgets():
+    return Item('Смартфон', 10000, 100)
 
 
 def test_item_str():
@@ -21,5 +27,9 @@ def test_discount():
     assert Item.apply_discount(item2) == 17000
 
 
+def test_repr(gadgets):
+    assert f"Item('Смартфон', 10000, 100)"
 
 
+def test_str(gadgets):
+    assert 'Смартфон'
