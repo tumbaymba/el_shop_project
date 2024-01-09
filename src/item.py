9a06c4bb27936e2875e1d5585422ba7ec287eb5a
Dmitry Pavlushin <pavlushindv@gmail.com>
2024-01-09 18:10:17 +0300

    HW 13.2 correct 4

--- src/item.py
+++ src/item.py
@@ -8,12 +8,13 @@ class Item:
     """
     pay_rate = 0.85
     all = []
-    file = "../items.csv"
+    #file = "..src/items.csv"
 
     def __init__(self, name: str, price: float, quantity: int):
         self.__name = name
         self.price = price
         self.quantity = quantity
+        Item.all.append(self)
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
@@ -21,9 +22,10 @@ class Item:
     def __str__(self):
         return f'{self.__name}'
 
+    file = "..src/items.csv"
     @classmethod
-    def instantiate_from_csv(cls, s):
-        with open(os.path.join(os.path.dirname(__file__), 'items.csv'), 'r', newline='') as csvfile:
+    def instantiate_from_csv(cls) -> None:
+        with open(os.path.join(os.path.dirname(__file__), 'src/items.csv'), 'r', newline='', encoding='cp1251') as csvfile:
             data = csv.DictReader(csvfile)
             product: dict
             for product in data:
@@ -38,14 +40,21 @@ class Item:
         return self.__name
 
     @name.setter
-    def name(self, name):
-        if len(name) <= 10:
-            self.__name = name
-        elif len(name) > 10:
-            print("Количество букв в наименовании больше 10")
+    def name(self, len_name):
+        if len(len_name) <= 10:
+            self.__name = len_name
+        elif len(len_name) > 10:
+            raise ValueError("Количество букв в наименовании больше 10")
+        self.__name = len_name
+
 
     def calculate_total_price(self):
         return self.price * self.quantity
 
     def apply_discount(self):
         self.price = self.price * self.pay_rate
+        return self.price
+
+all = Item.all
+
+#print(Item.all)
--- tests/test_item.py
+++ tests/test_item.py
@@ -3,21 +3,26 @@ import pytest
 
 from src.item import Item
 
+item = Item("Смартфон", 10000, 100)
+
 
 def test_item_str():
-    item = Item("Смартфон", 10000, 100)
+    #item = Item("Смартфон", 10000, 100)
     assert str(item) == 'Смартфон'
 
 
 def test_calculation():
-    item = Item("Смартфон", 10000, 100)
+    #item = Item("Смартфон", 10000, 100)
     assert item.calculate_total_price() == 1000000
 
 
 def test_discount():
-    item = Item("Смартфон", 10000, 100)
-    item.pay_rate = 0.85
-    assert item.price == 8500
+    #item = Item("Смартфон", 10000, 100)
+    #item.pay_rate = 0.85
+    #assert item.price == 8500
+
+    assert Item.apply_discount(item) == 8500.0
+
 
 
 def test_repr():
@@ -32,8 +37,8 @@ def test_name_setter():
 
 
 def test_instantiate_from_csv():
-    Item.instantiate_from_csv('..items.csv')
-
+    #Item.instantiate_from_csv("src/items.csv")
+    #assert len(Item.instantiate_from_csv("src/items.csv")) == 5
     assert len(Item.all) == 5
     assert Item.all[0].name == "Смартфон"
     assert Item.all[2].price == 10000
